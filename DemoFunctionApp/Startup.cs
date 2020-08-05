d7433ca15b20fddd2c921b6a709cc8f392bdf63d
Chandrashekhar Paliwal <C.Paliwal@wolterskluwer.com>
2020-08-05 13:58:59 +0530

    Added startup file to solve our dependancies

---
+++ DemoFunctionApp/Startup.cs
@@ -0,0 +1,15 @@
+﻿using DemoFunctionApp;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: FunctionsStartup(typeof(Startup))]
+namespace DemoFunctionApp
+{
+    public class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            builder.Services.AddScoped<IEmployeeService, EmployeeService>();
+        }
+    }
+}
